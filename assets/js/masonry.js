2cf996b7cc93a9afeb572162ceaad61d7385d21e
GitHub Action <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-06 12:25:15 +0000

    minor corrections about.md [ci skip]

--- assets/js/masonry.js
+++ assets/js/masonry.js
@@ -1,12 +1,1 @@
-$(document).ready(function() {
-  // Init Masonry
-  var $grid = $('.grid').masonry({
-    gutter: 10,
-    horizontalOrder: true,
-    itemSelector: '.-item',
-  });
-  // Layout Masonry after each image loads
-  $.imagesLoaded().progress( function() {
-    $.masonry('layout');
-  });
-});
+$(document).ready(function(){$(".grid").masonry({gutter:10,horizontalOrder:!0,itemSelector:".-item"});$.imagesLoaded().progress(function(){$.masonry("layout")})});
